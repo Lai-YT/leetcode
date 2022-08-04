2f8dc910d963053cd27ad7f2ea570a33b7b93c17
Lai-YT <381xvmvbib@gmail.com>
2022-08-04 20:53:49 +0800

    31 ms

--- solutions/494. Target Sum.cpp
+++ solutions/494. Target Sum.cpp
@@ -1,33 +1,36 @@
+/* formatted with `clang-format --style=llvm` */
+​
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        nums_ = nums;
-        target_ = target;
-        count_ = 0;
-        DepthFirstSearch_(nums.size() - 1, 0);
-        return count_;
-    }
+  int findTargetSumWays(vector<int> &nums, int target) {
+    nums_ = nums;
+    total_ = std::accumulate(nums.begin(), nums.end(), 0);
+    memo_ = std::vector(
+        nums.size(),
+        std::vector(total_ * 2 +
+                        1, /* to keep sum_ + total_ non-negative and in-range */
+                    PLACE_HOLDER_));
+    target_ = target;
+    return depthFirstSearch_(0, 0);
+  }
 ​
 private:
-    std::vector<int> nums_{};
-    int target_{};
-    int count_{};
+  std::vector<int> nums_{};
+  std::vector<std::vector<int>> memo_{};
+  int total_{};
+  int target_{};
+  const int PLACE_HOLDER_{-1}; /* as the count in memo_ can't be negative */
 ​
-    void DepthFirstSearch_(const size_t pos, const int sum) {
-        if (pos == 0) {
-            if (sum + nums_.at(pos) == target_) {
-                ++count_;
-            }
-            if (sum - nums_.at(pos) == target_) {
-                ++count_;
-            }
-            return;
-        }
-        int curr = nums_.at(pos);
-        DepthFirstSearch_(pos - 1, sum + curr);
-        DepthFirstSearch_(pos - 1, sum - curr);
+  int depthFirstSearch_(const size_t pos, const int sum) {
+    if (pos == nums_.size()) {
+      if (sum == target_) {
+        return 1;
+      }
+      return 0;
     }
-};
-​
-​
-​
+    if (!isMemoized_(memo_.at(pos).at(total_ + sum))) {
+      const int curr = nums_.at(pos);
+      memo_.at(pos).at(total_ + sum) = depthFirstSearch_(pos + 1, sum + curr) +
+                                       depthFirstSearch_(pos + 1, sum - curr);
+    }
+    return memo_.at(pos).at(total_ + sum);
