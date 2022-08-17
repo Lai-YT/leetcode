019829e02ddf8f4ea3cd42644ad47fbe4685b8f3
Lai-YT <381xvmvbib@gmail.com>
2022-08-17 21:55:01 +0800

    3 ms

---
+++ solutions/804. Unique Morse Code Words.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        for (const std::string& word : words) {
+            std::string transformation = transformWord_(word);
+            /* the method should be named `insertIfNotContain` */
+            uniqueTransformations_.insert(transformation);
+        }
+        return uniqueTransformations_.size();
+    }
+​
+private:
+    std::unordered_set<std::string> uniqueTransformations_{};
+    const std::array<std::string, 26> letterOrderToMorseCode_{
+        ".-",
+        "-...",
+        "-.-.",
+        "-..",
+        ".",
+        "..-.",
+        "--.",
+        "....",
+        "..",
+        ".---",
+        "-.-",
+        ".-..",
+        "--",
+        "-.",
+        "---",
+        ".--.",
+        "--.-",
+        ".-.",
+        "...",
+        "-",
+        "..-",
+        "...-",
+        ".--",
+        "-..-",
+        "-.--",
+        "--..",
+    };
+​
+    std::string transformWord_(const std::string& word) const {
+        std::string transformation{};
+        for (const char letter : word) {
+            const int letterOrder = getLowerCaseLetterOrder_(letter);
+            transformation += letterOrderToMorseCode_.at(letterOrder);
