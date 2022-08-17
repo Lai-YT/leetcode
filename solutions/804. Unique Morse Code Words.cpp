ed3a9593f45884cb45a5170b2b1c7699929137fc
Lai-YT <381xvmvbib@gmail.com>
2022-08-17 21:59:36 +0800

    0 ms

--- solutions/804. Unique Morse Code Words.cpp
+++ solutions/804. Unique Morse Code Words.cpp
@@ -12,25 +12,31 @@ public:
 private:
     std::unordered_set<std::string> uniqueTransformations_{};
     const std::array<std::string, 26> letterOrderToMorseCode_{
-        ".-",
-        "-...",
-        "-.-.",
-        "-..",
-        ".",
-        "..-.",
-        "--.",
-        "....",
-        "..",
-        ".---",
-        "-.-",
-        ".-..",
-        "--",
-        "-.",
-        "---",
-        ".--.",
-        "--.-",
-        ".-.",
-        "...",
-        "-",
-        "..-",
-        "...-",
+        ".-", "-...",
+        "-.-.", "-..",
+        ".", "..-.",
+        "--.", "....",
+        "..", ".---",
+        "-.-", ".-..",
+        "--", "-.",
+        "---", ".--.",
+        "--.-", ".-.",
+        "...", "-",
+        "..-", "...-",
+        ".--", "-..-",
+        "-.--", "--..",
+    };
+​
+    std::string transformWord_(const std::string& word) const {
+        std::string transformation{};
+        for (const char letter : word) {
+            const int letterOrder = getLowerCaseLetterOrder_(letter);
+            transformation += letterOrderToMorseCode_.at(letterOrder);
+        }
+        return transformation;
+    }
+​
+    /* a is 0, z is 25, ... */
+    int getLowerCaseLetterOrder_(const char lowerCaseLetter) const {
+        return lowerCaseLetter - 97;
+    }
