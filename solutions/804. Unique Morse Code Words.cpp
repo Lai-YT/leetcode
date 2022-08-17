cf8c2012a73b9cb2b557b7f9bb7a69904ea095f0
Lai-YT <381xvmvbib@gmail.com>
2022-08-17 22:03:23 +0800

    fix: missing code section
    This could be a bug of https://github.com/Jintin/LeetRecord

--- solutions/804. Unique Morse Code Words.cpp
+++ solutions/804. Unique Morse Code Words.cpp
@@ -1,42 +1,57 @@
 class Solution {
 public:
-    int uniqueMorseRepresentations(vector<string>& words) {
-        for (const std::string& word : words) {
-            std::string transformation = transformWord_(word);
-            /* the method should be named `insertIfNotContain` */
-            uniqueTransformations_.insert(transformation);
-        }
-        return uniqueTransformations_.size();
-    }
-​
+    int uniqueMorseRepresentations(vector<string>& words) {
+        for (const std::string& word : words) {
+            std::string transformation = transformWord_(word);
+            /* the method should be named `insertIfNotContain` */
+            uniqueTransformations_.insert(transformation);
+        }
+        return uniqueTransformations_.size();
+    }
+
 private:
-    std::unordered_set<std::string> uniqueTransformations_{};
-    const std::array<std::string, 26> letterOrderToMorseCode_{
-        ".-", "-...",
-        "-.-.", "-..",
-        ".", "..-.",
-        "--.", "....",
-        "..", ".---",
-        "-.-", ".-..",
-        "--", "-.",
-        "---", ".--.",
-        "--.-", ".-.",
-        "...", "-",
-        "..-", "...-",
-        ".--", "-..-",
-        "-.--", "--..",
-    };
-​
-    std::string transformWord_(const std::string& word) const {
-        std::string transformation{};
-        for (const char letter : word) {
-            const int letterOrder = getLowerCaseLetterOrder_(letter);
-            transformation += letterOrderToMorseCode_.at(letterOrder);
-        }
-        return transformation;
-    }
-​
-    /* a is 0, z is 25, ... */
-    int getLowerCaseLetterOrder_(const char lowerCaseLetter) const {
-        return lowerCaseLetter - 97;
-    }
+    std::unordered_set<std::string> uniqueTransformations_{};
+    const std::array<std::string, 26> letterOrderToMorseCode_{
+        ".-",
+        "-...",
+        "-.-.",
+        "-..",
+        ".",
+        "..-.",
+        "--.",
+        "....",
+        "..",
+        ".---",
+        "-.-",
+        ".-..",
+        "--",
+        "-.",
+        "---",
+        ".--.",
+        "--.-",
+        ".-.",
+        "...",
+        "-",
+        "..-",
+        "...-",
+        ".--",
+        "-..-",
+        "-.--",
+        "--..",
+    };
+
+    std::string transformWord_(const std::string& word) const {
+        std::string transformation{};
+        for (const char letter : word) {
+            const int letterOrder = getLowerCaseLetterOrder_(letter);
+            transformation += letterOrderToMorseCode_.at(letterOrder);
+        }
+        return transformation;
+    }
+
+    /* a is 0, z is 25, ... */
+    int getLowerCaseLetterOrder_(const char lowerCaseLetter) const {
+        return lowerCaseLetter - 97;
+    }
+
+};
