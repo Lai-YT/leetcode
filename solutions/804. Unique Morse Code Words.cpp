d42e1e74339e3d010d6648efdec7f9696ee4a193
Lai-YT <381xvmvbib@gmail.com>
2022-08-17 21:56:53 +0800

    3 ms

--- solutions/804. Unique Morse Code Words.cpp
+++ solutions/804. Unique Morse Code Words.cpp
@@ -1,3 +1,13 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        for (const std::string& word : words) {
+            std::string transformation = transformWord_(word);
+            /* the method should be named `insertIfNotContain` */
+            uniqueTransformations_.insert(transformation);
+        }
+        return uniqueTransformations_.size();
+    }
 ​
 private:
     std::unordered_set<std::string> uniqueTransformations_{};
