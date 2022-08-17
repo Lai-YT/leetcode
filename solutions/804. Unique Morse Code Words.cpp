91d95b5e69c81750f432db5ea8fe493893ce2c49
Lai-YT <381xvmvbib@gmail.com>
2022-08-17 21:57:59 +0800

    0 ms

--- solutions/804. Unique Morse Code Words.cpp
+++ solutions/804. Unique Morse Code Words.cpp
@@ -34,25 +34,3 @@ private:
         "-",
         "..-",
         "...-",
-        ".--",
-        "-..-",
-        "-.--",
-        "--..",
-    };
-​
-    std::string transformWord_(const std::string& word) const {
-        std::string transformation{};
-        for (const char letter : word) {
-            const int letterOrder = getLowerCaseLetterOrder_(letter);
-            transformation += letterOrderToMorseCode_.at(letterOrder);
-        }
-        return transformation;
-    }
-​
-    /* a is 0, z is 25, ... */
-    int getLowerCaseLetterOrder_(const char lowerCaseLetter) const {
-        return lowerCaseLetter - 97;
-    }
-​
-};
-​
