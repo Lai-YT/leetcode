14af256d4302506524678b3ab46d17ca72c4b8ea
Lai-YT <381xvmvbib@gmail.com>
2023-01-27 13:41:41 +0000

    refactor: renovate syntax

--- solutions/1. Two Sum.cpp
+++ solutions/1. Two Sum.cpp
@@ -1,12 +1,15 @@
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> m;
-        for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums.at(i);
-            if (m.count(complement) != 0) return vector<int> {i, m[complement]};
-            else m[nums.at(i)] = i;
-        }
-        return vector<int>();
-    }
+ public:
+  std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::map<int, int> m{};
+    for (int i = 0; i < nums.size(); i++) {
+      const int complement = target - nums.at(i);
+      if (m.count(complement) != 0) {
+        return {i, m.at(complement)};
+      } else {
+        m[nums.at(i)] = i;
+      }
+    }
+    return {};
+  }
 };
