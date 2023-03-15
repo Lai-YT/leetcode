f0ac1269dffe20828931fcfc49fdddf29563cf67
Lai-YT <381xvmvbib@gmail.com>
2023-03-15 15:27:48 +0800

    fix: indention & invalid character

--- solutions/136. Single Number.c
+++ solutions/136. Single Number.c
@@ -1,9 +1,7 @@
-​
-​
-int singleNumber(int* nums, int numsSize){
-    int check = nums[0];
-    for (int i = 1; i < numsSize; ++i) {
-        check ^= nums[i];
-    }
-    return check;
+int singleNumber(int* nums, int numsSize) {
+    int check = 0;
+    for (int i = 0; i < numsSize; ++i) {
+        check ^= nums[i];
+    }
+    return check;
 }
