f3143e0926917a033583eeee66382eddae12a865
Lai-YT <381xvmvbib@gmail.com>
2023-06-30 13:52:24 +0800

    1970. Last Day Where You Can Still Cross, 43 ms (Rust)
    Reduce time complexity with union-find.

--- solutions/1970. Last Day Where You Can Still Cross.rs
+++ solutions/1970. Last Day Where You Can Still Cross.rs
@@ -1,10 +1,17 @@
+struct UnionFind {
+    roots: Vec<usize>,
+    ranks: Vec<usize>,
+}
+
 impl Solution {
     const LAND: i32 = 0;
     const WATER: i32 = 1;
+    const MOVES: [(i32, i32); 4] = [(1, 0), (0, -1), (-1, 0), (0, 1)];
 
-    /// Depth-first search on days + using binary search to skip some days.
-    /// - Time complexity: O(row * col * log(row * col)). O(row * col) for each depth-first search, O(log(`cells.len()`) for the binary search, where `cells.len()` is row * col)
-    /// - Space complexity: O(row * col).
+    /// Union-find on land.
+    /// - Time complexity: O(row * col). At most row * col waters to resolve
+    /// and each operation on `UnionFind` can be considered as O(1).
+    /// - Space complexity: O(row * col). `UnionFind` with all cells.
     pub fn latest_day_to_cross(row: i32, col: i32, cells: Vec<Vec<i32>>) -> i32 {
         // Resolve the nasty types.
         let row = row as usize;
@@ -13,56 +20,43 @@ impl Solution {
             .iter()
             .map(|c| (c[0] as usize, c[1] as usize))
             .collect::<Vec<_>>();
-        // lower bound: the latest day we currently know that can cross.
-        // upper bound: the farthest day that might cross.
-        let mut day_range = (0usize, waters.len());
-        while day_range.0 != day_range.1 {
-            let day = (day_range.0 + day_range.1 + 1/* so we don't get stuck when `day_range.1 == day_range.0 + 1` */)
-                / 2;
-            if Self::can_cross(row, col, &waters[0..day]) {
-                // Adjust the latest day that can cross to `day`.
-                day_range = (day, day_range.1);
-            } else {
-                // `day` is checked to not cross
-                // Adjust the farthest day that might cross to `day - 1`.
-                day_range = (day_range.0, day - 1);
-            }
-        }
-        day_range.0 as i32
-    }
-
-    const MOVES: [(i32, i32); 4] = [(1, 0), (0, -1), (-1, 0), (0, 1)];
-    const VISITED: i32 = -1;
-
-    /// - Time complexity: O(row * col); build the matrix is O(row * col), depth-first search is also O(number of land), which is also O(row * col).
-    /// - Space complexity: O(row * col).
-    fn can_cross(row: usize, col: usize, waters: &[(usize, usize)]) -> bool {
+        // NOTE: 2 extra spaces to indicate the top row group and the bottom row group,
+        // once they are connected, we find the cross.
+        let mut union_find = UnionFind::new(row * col + 2);
+        // Since the `union_find` is 1-dimensional, we'll have to perform a mapping.
+        // (1, 1) has to be at 1, (row, col) has to be at `row * col`.
         // NOTE: 1-based index
-        let mut matrix = vec![vec![Self::LAND; col + 1]; row + 1];
-        for &(r, c) in waters {
-            matrix[r][c] = Self::WATER;
-        }
-        let mut stack = Vec::<(usize, usize)>::new();
-        // The sources are the lands on the top row, the destinations are any land on the bottom row.
-        // Start from the sources.
-        for j in 1..=col {
-            if matrix[1][j] == Self::LAND {
-                stack.push((1, j));
-            }
-        }
-        while let Some(land) = stack.pop() {
+        let map_index = |cell: (usize, usize)| (cell.0 - 1) * col + (cell.1 - 1) + 1;
+        // index 0 and index `row * col + 1` is used as the top and bottom group
+        let top_group = 0;
+        let bottom_group = row * col + 1;
+        // Using the union-find data structure, we are to find the first day
+        // which the top row and the bottom row is connected.
+        // Start from the last day, where there are no more lands.
+        let mut matrix = vec![vec![Self::WATER; col + 1]; row + 1];
+        for (day, &new_land) in waters.iter().enumerate().rev() {
+            // Flip water to land, connect to its neighbor lands.
+            matrix[new_land.0][new_land.1] = Self::LAND;
             for r#move in Self::MOVES {
-                if let Some(next_land) = Self::move_to_land(land, r#move, &matrix) {
-                    // Reach any of the destinations.
-                    if next_land.0 == row {
-                        return true;
-                    }
-                    matrix[next_land.0][next_land.1] = Self::VISITED;
-                    stack.push(next_land);
+                if let Some(neighbor_land) = Self::move_to_land(new_land, r#move, &matrix) {
+                    union_find.union(map_index(new_land), map_index(neighbor_land));
                 }
             }
+            // Also if the land is in the top or bottom row, we'll add them into the extra group.
+            if new_land.0 == 1 {
+                union_find.union(map_index(new_land), top_group);
+            }
+            if new_land.0 == row {
+                union_find.union(map_index(new_land), bottom_group);
+            }
+            // NOTE: At `day`, the `water` is still there, so the result should be `day - 1`,
+            // but because `waters` is 0-based (yes, although others are 1-based),
+            // the result is `day`.
+            if union_find.is_connected(top_group, bottom_group) {
+                return day as i32;
+            }
         }
-        false
+        unreachable!()
     }
 
     /// Returns the moved cell if such cell is in the matrix and is a piece of land.
@@ -73,22 +67,61 @@ impl Solution {
         offset: (i32, i32),
         matrix: &Vec<Vec<i32>>,
     ) -> Option<(usize, usize)> {
-        if cell.0 == 1 && offset.0 == -1
-            || cell.1 == 1 && offset.1 == -1
-            || cell.0 == matrix.len() - 1 && offset.0 == 1
-            || cell.1 == matrix[1].len() - 1 && offset.1 == 1
+        // Since it's 1-based, won't overflow on the attempt.
+        let next = (
+            (cell.0 as i32 + offset.0) as usize,
+            (cell.1 as i32 + offset.1) as usize,
+        );
+        if next.0 == 0
+            || next.1 == 0
+            || next.0 == matrix.len()
+            || next.1 == matrix[0].len()
+            || matrix[next.0][next.1] == Self::WATER
         {
             None
         } else {
-            let next = (
-                (cell.0 as i32 + offset.0) as usize,
-                (cell.1 as i32 + offset.1) as usize,
-            );
-            if matrix[next.0][next.1] == Self::LAND {
-                Some(next)
+            Some(next)
+        }
+    }
+}
+
+impl UnionFind {
+    pub fn new(size: usize) -> Self {
+        Self {
+            // initialize the root as the node itself
+            roots: (0..size).collect(),
+            ranks: vec![1 /* starts with rank 1 */; size],
+        }
+    }
+
+    pub fn is_connected(&mut self, a: usize, b: usize) -> bool {
+        self.find(a) == self.find(b)
+    }
+
+    /// O(1) amortized. Optimized with union by rank.
+    pub fn union(&mut self, a: usize, b: usize) {
+        let root_a = self.find(a);
+        let root_b = self.find(b);
+        if root_a != root_b {
+            if self.ranks[root_a] > self.ranks[root_b] {
+                self.roots[root_b] = root_a;
+            } else if self.ranks[root_a] < self.ranks[root_b] {
+                self.roots[root_a] = root_b;
             } else {
-                None
+                // choose `root_a` as the root, doesn't matter
+                self.roots[root_b] = root_a;
+                self.ranks[root_a] += 1;
             }
         }
+    }
+
+    /// Returns the root node of `node`.
+    /// O(1) amortized. Optimized with path compression.
+    fn find(&mut self, node: usize) -> usize {
+        if self.roots[node] == node {
+            return node;
+        }
+        self.roots[node] = self.find(self.roots[node]);
+        self.roots[node]
     }
 }
