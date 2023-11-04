82f858ccabdd7e767946e630ac6db6e245096ee3
Lai-YT <381xvmvbib@gmail.com>
2023-11-05 00:47:35 +0800

    743. Network Delay Time, 76 ms (C++)
    Time complexity reduced by using Dijkstra's algorithm instead of
    Bellman-Ford algorithm.

--- solutions/743. Network Delay Time.cpp
+++ solutions/743. Network Delay Time.cpp
@@ -1,11 +1,14 @@
 #include <algorithm>
+#include <functional>
 #include <iostream>
 #include <limits>
+#include <queue>
+#include <utility>
 #include <vector>
 
 class Solution {
 public:
-  /// @details Using Bellman-Ford algorithm to find the shortest paths from the
+  /// @details Using Dijkstra's algorithm to find the shortest paths from the
   /// source to every other nodes.
   int networkDelayTime(std::vector<std::vector<int>> &times, int n, int k) {
     //
@@ -18,22 +21,49 @@ public:
     // init source
     distances[NodeNoToIndex_(k)] = 0;
 
+    // Each node is visited only once. The shortest path can be obtained in this
+    // single visitation greedily.
+    bool visits[n];
+    std::fill(visits, visits + n, false);
+
+    auto edges =
+        std::vector<std::vector<std::pair<int /* neighbor */, int /* time */>>>(
+            n);
+    for (auto &edge : times) {
+      auto from = edge.at(0);
+      auto to = edge.at(1);
+      auto time = edge.at(2);
+      edges.at(NodeNoToIndex_(from)).emplace_back(to, time);
+    }
+
     //
-    // Relax all edges n - 1 times.
+    // Relax all edges.
+    // NOTE: since we do not return early when reaching a specific node, we get
+    // the shortest path to all other nodes from the source.
     //
 
-    for (auto _ = 0; _ < n - 1; _++) {
-      for (auto &edge : times) {
-        auto from = edge.at(0);
-        auto to = edge.at(1);
-        auto time = edge.at(2);
+    auto min_heap =
+        std::priority_queue<std::pair<int /* distance */, int /* to */>,
+                            std::vector<std::pair<int, int>>,
+                            std::greater<std::pair<int, int>>>{};
+    min_heap.emplace(0, k);
+    while (!min_heap.empty()) {
+      auto [_, to] = min_heap.top();
+      min_heap.pop();
+      if (visits[NodeNoToIndex_(to)]) {
+        continue;
+      }
+      visits[NodeNoToIndex_(to)] = true;
+      for (auto [neighbor, time] : edges.at(NodeNoToIndex_(to))) {
         // NOTE: be careful for the overflow when the distance is infinity
-        auto alter_path = distances[NodeNoToIndex_(from)] == kInfinity
+        auto alter_path = distances[NodeNoToIndex_(to)] == kInfinity
                               ? kInfinity
-                              : distances[NodeNoToIndex_(from)] + time;
-        if (alter_path < distances[NodeNoToIndex_(to)]) {
+                              : distances[NodeNoToIndex_(to)] + time;
+        if (alter_path < distances[NodeNoToIndex_(neighbor)]) {
           // getting to edge.to through node i is shorter
-          distances[NodeNoToIndex_(to)] = alter_path;
+          distances[NodeNoToIndex_(neighbor)] = alter_path;
+          // keep exploring in this way
+          min_heap.emplace(distances[NodeNoToIndex_(neighbor)], neighbor);
         }
       }
     }
