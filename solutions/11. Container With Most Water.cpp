322ba42a782125ab93f97e10e27a9e57625b5170
Lai-YT <381xvmvbib@gmail.com>
2026-05-02 16:59:28 +0800

    11. Container With Most Water, 0 ms (C++)

--- solutions/11. Container With Most Water.cpp
+++ solutions/11. Container With Most Water.cpp
@@ -1,45 +1,16 @@
 class Solution {
- public:
-  int maxArea(std::vector<int>& height) {
-    heights_ = std::move(height);
-    left_ = 0;
-    right_ = heights_.size() - 1;
-    return MaxArea_();
-  }
-
- private:
-  using Index = int;
-
-  std::vector<int> heights_;
-  Index left_;
-  Index right_;
-
-  int MaxArea_() {
-    int max = 0;
-    while (!Crossed_()) {
-      max = std::max(max, CalculateWater_());
-      /* OPTIMIZATION: less calculation on water.
-        If you don't find level higher than this,
-        you're not gonna obtain more water,
-        because the width is getting smaller. */
-      const int water_level = WaterLevel_();
-      while (!Crossed_() && heights_.at(left_) <= water_level) {
-        ++left_;
-      }
-      while (!Crossed_() && heights_.at(right_) <= water_level) {
-        --right_;
-      }
+public:
+    int maxArea(std::vector<int>& height) {
+        int s = 0, e = height.size() - 1;
+        int m = 0;
+        while (s < e) {
+            m = std::max(m, std::min(height[s], height[e]) * (e - s));
+            if (height[s] < height[e]) {
+                ++s;
+            } else {
+                --e;
+            }
+        }
+        return m;
     }
-    return max;
-  }
-
-  bool Crossed_() const { return Width_() < 0; }
-
-  int CalculateWater_() const { return WaterLevel_() * Width_(); }
-
-  int Width_() const { return right_ - left_; }
-
-  int WaterLevel_() const {
-    return std::min(heights_.at(left_), heights_.at(right_));
-  }
 };
